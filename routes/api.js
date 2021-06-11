961aacef6f28a2b4f5d304ea2d2796ffd2854adc
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-06-10 21:33:45 -0700

    Figured out the 2 primary routes and what they're doing

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {Workout} = require('../models/index.js');
-
+//still unsure what exactly is wrong with this one?
 router.get('/workouts' , async (req,res)=>{
     console.log('***route hit***');
    try{ 
@@ -13,7 +13,7 @@ router.get('/workouts' , async (req,res)=>{
        return console.error(err);
    }
 });
-
+//adding an exercise to a specific day's exercises array
 router.put('/workouts/:id' , async (req , res) => {
     try{
         console.log('This is adding a workout???');
@@ -24,7 +24,7 @@ router.put('/workouts/:id' , async (req , res) => {
         return console.error(err);
     }
 });
-
+//creating a day plan workout with an exercise
 router.post('/workouts', async (req, res) =>{
     console.log('***Adding a workout***');
     console.log(req.body);
@@ -35,17 +35,17 @@ router.post('/workouts', async (req, res) =>{
         return console.error(err);
     }
 });
-//????
+
 router.get('/workouts/range' , (req , res) => {
     try{
         Workout.aggregate([
             {
                 $addFields: {
-                    totalDuration: {$sum : 'duration'},
-                    totalWeight: {$sum: 'weight'}
+                    totalDuration: {$sum : '$exercises.duration'},
+                    totalWeight: {$sum: '$exercises.weight'}
                 }
             }
-        ]).then(data => {
+        ]).sort({day: -1}).limit(7).then(data => {
             res.json(data);
         })
     } catch (err) {
